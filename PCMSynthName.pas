6e15f25825c747375fdf761e90e1e1ab1bd6e361
BerndPeuser <BerndPeuser@users.noreply.github.com>
2014-08-05 12:19:09 +0200

    Update PCMSynthName.pas

--- PCMSynthName.pas
+++ PCMSynthName.pas
@@ -1,36 +1,56 @@
 function PCMSynthName (num : integer) : String;
+//Manual page 40
 begin
-	Case num of 
+	Case num of
+		//Keyboard
 		1 : PCMSynthName='Piano';
 		2 : PCMSynthName='E-Piano';
 		3 : PCMSynthName='Clav';
 		4 : PCMSynthName='M1-Organ';
-		5 : PCMSynthName='';
+		5 : PCMSynthName='Organ';
+		//Mallet
 		6 : PCMSynthName='';
 		7 : PCMSynthName='';
 		8 : PCMSynthName='';
+		//Woodwind
 		9 : PCMSynthName='';
 		10 : PCMSynthName='';
 		11 : PCMSynthName='';
+		//Brass
 		12 : PCMSynthName='';
 		13 : PCMSynthName='';
 		14 : PCMSynthName='';
+		//Choir
 		15 : PCMSynthName='';
 		16 : PCMSynthName='';
 		17 : PCMSynthName='';
+		//String
 		18 : PCMSynthName='';
 		19 : PCMSynthName='';
 		20 : PCMSynthName='';
+		//Guitar
 		21 : PCMSynthName='';
 		22 : PCMSynthName='';
 		23 : PCMSynthName='';
 		24 : PCMSynthName='';
 		25 : PCMSynthName='';
+		//Bass
 		26 : PCMSynthName='';
 		27 : PCMSynthName='';
 		28 : PCMSynthName='';
 		29 : PCMSynthName='';
 		30 : PCMSynthName='';
+		//SynthWave
+		31 : PCMSynthName='';
+		32 : PCMSynthName='';
+		33 : PCMSynthName='';
+		34 : PCMSynthName='';
+		35 : PCMSynthName='';
+		36 : PCMSynthName='';
+		37 : PCMSynthName='';
+		38 : PCMSynthName='';
+		39 : PCMSynthName='';
+		40 : PCMSynthName='';
 		41 : PCMSynthName='';
 		42 : PCMSynthName='';
 		43 : PCMSynthName='';
@@ -44,8 +64,10 @@ begin
 		51 : PCMSynthName='';
 		52 : PCMSynthName='';
 		53 : PCMSynthName='';
+		//Motion
 		54 : PCMSynthName='';
 		55 : PCMSynthName='';
+		//Hit
 		56 : PCMSynthName='';
 		57 : PCMSynthName='';
 		58 : PCMSynthName='';
@@ -56,15 +78,20 @@ begin
 		63 : PCMSynthName='';
 		64 : PCMSynthName='';
 		65 : PCMSynthName='';
+		//Chordset (Maj,min,Maj7,min7)
 		66 : PCMSynthName='';
 		67 : PCMSynthName='';
 		68 : PCMSynthName='';
 		69 : PCMSynthName='';
 		70 : PCMSynthName='';
+		//Chordset (min,sus47,min7)
 		71 : PCMSynthName='';
+		//Chordset (min,min,sus4,)
 		72 : PCMSynthName='';
+		//Drumset
 		73 : PCMSynthName='';
 		74 : PCMSynthName='';
 		75 : PCMSynthName='';
-		76 : PCMSynthName='';		
+		76 : PCMSynthName='';
+	end;
 end;
